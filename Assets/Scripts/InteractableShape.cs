daa0037ffe0f4cdc63c1bdcead29b22cb1825c3a
Nora Dembowski <noradembowski@gmail.com>
2024-01-30 15:27:10 +0100

    Ass basic interactability to dummy window

---
+++ Assets/Scripts/InteractableShape.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableShape : MonoBehaviour
+{
+
+    public AudioSource grab, release;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetHover(bool hovering)
+    {
+        animator.SetBool("Hover", hovering);
+
+    }
+
+    public void SetClick(bool clicking)
+    {
+        animator.SetBool("Click", clicking);
+
+        if (clicking)
+        {
+            grab.Play();
+        }
+        else
+        {
+            release.Play();
+        }
+    }
+}
